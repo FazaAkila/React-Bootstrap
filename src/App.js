4c69a520fbadf4f3cb06c4d6927986090a5200b7
FazaAkila <79125027+FazaAkila@users.noreply.github.com>
2021-09-27 18:51:02 +0700

    Project React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import { Table } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br />
+      <br />
+      <h1>React Bootstrap</h1>
+      <br />
+      <div className="col-sm-8 offset-sm-2">
+        <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Nama</th>
+              <th>Umur</th>
+              <th>Tempat, Tanggal Lahir</th>
+              <th>Alamat</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Alexa Arianna</td>
+              <td>17 Tahun</td>
+              <td>Malang, 8 Desembar 2003</td>
+              <td>St. Elm Northside Brooklyn West</td>
+              <td>@heyitslexi</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Reynold Zachy</td>
+              <td>17 Tahun</td>
+              <td>Malang, 21 Februari 2003</td>
+              <td>St. Elm Northside The Bridge</td>
+              <td>@reyzachy</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Victoria Sydney</td>
+              <td>17 Tahun</td>
+              <td>Malang, 29 Juni 2003</td>
+              <td>St. Elm Northside Brigerton Out Wood</td>
+              <td>@victorious</td>
+            </tr>
+            <tr>
+              <td>4</td>
+              <td>King Marco</td>
+              <td>17 Tahun</td>
+              <td>Kalimantan, 27 Oktober 2003</td>
+              <td>St. Elm Northside Brooklyn Est</td>
+              <td>@iamtheking</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
     </div>
   );
 }
